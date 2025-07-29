51a52dffd467a787ec2f5a4ed898e1a39189bcc6
jaime <jverdin@westmont.edu>
2025-07-28 18:05:02 -0700

    Added text count function to get the result

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
-print("greetings boots")
+def main():
+    book_path = "books/frankenstein.txt"
+    text = get_book_text(book_path)
+    print(text)
+    result = text_count(text)
+    print(result)
+
+def get_book_text(path):
+    with open(path) as f:
+        return f.read()
+
+
+def text_count (book):
+    num_words = 0
+    words = book.split()
+    for word in words:
+        num_words += 1
+    return f"{num_words} words found in the document"
+
+main()
