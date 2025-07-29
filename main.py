51cffae26186eaf8682de96403721e5f990e20c2
jaime <jverdin@westmont.edu>
2025-07-28 18:37:00 -0700

    added the stats.py folder

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
+from stats import text_count
+
+def get_book_text(path):
+    with open(path) as f:
+        return f.read()
 def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
@@ -5,16 +10,5 @@ def main():
     result = text_count(text)
     print(result)
 
-def get_book_text(path):
-    with open(path) as f:
-        return f.read()
-
-
-def text_count (book):
-    num_words = 0
-    words = book.split()
-    for word in words:
-        num_words += 1
-    return f"{num_words} words found in the document"
 
 main()
---
+++ stats.py
@@ -0,0 +1,3 @@
+def text_count (book):
+    num_words = len(book.split())
+    return f"{num_words} words found in the document"
